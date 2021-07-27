4fd57f83412e719599e56a70a3e5709f9d0e3c66
Bob Steers <steers.bob@gmail.com>
2021-07-28 00:18:23 +0200

    use sys thermal, and improve pi-temp comments

--- pi-temperature/publish-temp.sh
+++ pi-temperature/publish-temp.sh
@@ -2,21 +2,32 @@
 set -euo pipefail
 trap 'pkill -P $$; exit 1;' TERM INT
 
-HOST="172.17.0.1" # Default Docker host IP
-TOPIC="brewcast/history/pi-temperature"
-TEMPLATE='{ "key": "pi-temperature", "data": { "temperature[degC]": #TEMP } }'
-PATH="$PATH:/opt/vc/bin" # vcgencmd lives in /opt/vc/bin or /usr/bin
+# Default Docker host IP. Replace this if you're running on the host.
+HOST="172.17.0.1"
 
-# Get vcgencmd measure_temp value (example: temp=45.7'C)
-# Extract the numeric value with regex
-# Replace '#TEMP' string in the template with numeric value
-# Then publish the resulting JSON string as MQTT message
+# MQTT Topic for publishing Brewblox history.
+TOPIC="brewcast/history/pi-temperature"
+
+# We want to use a variable in a string that contains double quotes.
+# We build a template as single quoted string, and later replace '#TEMP' with the variable.
+# This avoids having to escape all double quotes in the string itself.
+TEMPLATE='{"key": "pi-temperature", "data": {"temperature[degC]": #TEMP}}'
+
 while :
 do
-    # call/wait sleep to handle SIGINT immediately
+    # Make sleep a background process, and wait for it.
+    # This handles SIGINT immediately when received.
     sleep 10 &
     wait $!
-    if [[ $(vcgencmd measure_temp) =~ temp=([0-9.]+) ]]; then
-        mosquitto_pub -h "$HOST" -t "$TOPIC" -m "${TEMPLATE//#TEMP/${BASH_REMATCH[1]}}" || true
-    fi
+
+    # Get temperature in millidegC.
+    TEMP_MILLI_C=$(cat /sys/class/thermal/thermal_zone0/temp)
+
+    # Convert temp to degC.
+    TEMP_C="$(perl -e "print ${TEMP_MILLI_C} / 1000")"
+
+    # Replace '#TEMP' string in the template with numeric value.
+    # Publish the JSON string as MQTT message.
+    # Ignore publish errors: maybe the eventbus is still starting.
+    mosquitto_pub -h "$HOST" -t "$TOPIC" -m "${TEMPLATE//#TEMP/${TEMP_C}}" || true
 done
